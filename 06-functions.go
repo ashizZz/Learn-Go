4b988e96c2a4a03be267d0613b62aeb6e25439d5
Ashish Bogati <60840304+ashizZz@users.noreply.github.com>
2022-07-26 09:34:31 +0545

    Update 06-functions.go

--- 06-functions.go
+++ 06-functions.go
@@ -8,7 +8,7 @@ import (
 const conferenceTickets int = 50
 
 var remainingTickets uint = 50
-var conferenceName = "Go Conference"
+var conferenceName = "ashizZz's Conference"
 var bookings = []string{}
 
 func main() {
